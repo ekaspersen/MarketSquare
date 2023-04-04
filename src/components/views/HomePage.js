025eb63dc5b7b9b6407b50dde682da7a8320d1b4
Eskil K. Hagen <eskilkaspersen@gmail.com>
2023-04-04 17:02:12 +0200

    wewe

--- src/components/views/HomePage.js
+++ src/components/views/HomePage.js
@@ -1,14 +1,12 @@
-import {useEffect, useMemo} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import {fetchProducts} from '../../store/modules/productsSlice';
-import {Link} from 'react-router-dom';
+import { useEffect, useMemo } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchProducts } from "../../store/modules/productsSlice";
+import { Link } from "react-router-dom";
 import ErrorComponent from "../../components/shared/ErrorComponent";
 
 function HomePage() {
     const dispatch = useDispatch();
-    const {products} = useSelector(state => state.products);
-    const {isError} = useSelector(state => state.error);
-    const {errorMessage} = useSelector(state => state.error);
+    const { products } = useSelector((state) => state.products);
 
     useEffect(() => {
         dispatch(fetchProducts());
@@ -23,34 +21,54 @@ function HomePage() {
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <div className="bg-white">
                 <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-                    <h2 className="text-2xl font-bold tracking-tight text-gray-900">Products</h2>
+                    <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+                        Products
+                    </h2>
                     {isError ? (
-                        <ErrorComponent message={errorMessage}/>
+                        <ErrorComponent message={errorMessage} />
                     ) : (
-                        <div
-                            className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                        <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                             {memoizedProducts.map((product) => (
-                                <div key={product.id} className="group relative flex flex-col gap-y-3">
-                                    <Link to={`product/${product.id}`}
-                                          className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
-                                        <img src={product.thumbnail} alt={product.title}
-                                             className="h-full w-full object-contain object-center lg:h-full lg:w-full"
-                                             loading="lazy"/>
+                                <div
+                                    key={product.id}
+                                    className="group relative flex flex-col gap-y-3"
+                                >
+                                    <Link
+                                        to={`product/${product.id}`}
+                                        className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80"
+                                    >
+                                        <img
+                                            src={product.thumbnail}
+                                            alt={product.title}
+                                            className="h-full w-full object-contain object-center lg:h-full lg:w-full"
+                                            loading="lazy"
+                                        />
                                     </Link>
                                     <div className="mt-4 flex justify-between">
                                         <div>
                                             <h3 className="text-md text-gray-700 relative">
-                                                <Link to={`product/${product.id}`}>
-                                                    <span aria-hidden="true" className="absolute inset-0"/>
+                                                <Link
+                                                    to={`product/${product.id}`}
+                                                >
+                                                    <span
+                                                        aria-hidden="true"
+                                                        className="absolute inset-0"
+                                                    />
                                                     {product.title}
                                                 </Link>
                                             </h3>
-                                            <p className="mt-1 text-sm text-gray-500">{product.description}</p>
+                                            <p className="mt-1 text-sm text-gray-500">
+                                                {product.description}
+                                            </p>
                                         </div>
-                                        <p className="text-sm font-medium text-gray-900">NOK{product.price}</p>
+                                        <p className="text-sm font-medium text-gray-900">
+                                            NOK{product.price}
+                                        </p>
                                     </div>
-                                    <Link to={`product/${product.id}`}
-                                          className="mt-4 py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-600 text-center">
+                                    <Link
+                                        to={`product/${product.id}`}
+                                        className="mt-4 py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-600 text-center"
+                                    >
                                         View
                                     </Link>
                                 </div>
